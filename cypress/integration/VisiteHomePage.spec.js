8af9b22a6c9901a18ff6218617e39952e8a6288f
talgat55 <lightxdesign55@gmail.com>
2020-01-31 01:14:20 +0600

    add next part test  check buttons and submit form

--- cypress/integration/VisiteHomePage.spec.js
+++ cypress/integration/VisiteHomePage.spec.js
@@ -3,11 +3,32 @@ describe('Home page', () =>{
         cy.visit('http://localhost:3000/');
    });
 
+    it('Home page  open modal by click on button in header ', () => {
+        cy.visit('http://localhost:3000/');
+        cy.get('header .feedback-link').click();
+        cy.get('.custom-modal-wrapper.active ');
+    });
+
+
+    it('Home page  check for modal send form', () => {
+        cy.visit('http://localhost:3000/');
+        cy.get('header .feedback-link').click();
+
+        cy.get('.custom-modal-wrapper.active  input[name=username]').type('test name');
+        cy.get('.custom-modal-wrapper.active   input[name=userphone]').type('79034567459');
+        cy.get('.custom-modal-wrapper.active   .btn-secondary').click();
+        cy.get('.custom-modal-wrapper.success.active');
+    });
+
+
     it('Home page work  Home slider ', () => {
         cy.visit('http://localhost:3000/');
         cy.get('.home-slider')
     });
-     it('Home page work  Service block ', () => {
+
+
+
+    it('Home page work  Service block ', () => {
         cy.visit('http://localhost:3000/');
         cy.get('.service-block .item-wrap');
     });
@@ -25,8 +46,14 @@ describe('Home page', () =>{
         cy.visit('http://localhost:3000/');
         cy.get('.news-block .item');
     });
+    it('Home page  open modal by click on button in block ur folks ', () => {
+        cy.visit('http://localhost:3000/');
+        cy.get('.info-block .link-alt').click();
+        cy.get('.custom-modal-wrapper.active ');
+    });
 
-     it('Home page work  Reviews block ', () => {
+
+    it('Home page work  Reviews block ', () => {
         cy.visit('http://localhost:3000/');
         cy.get('.reviews-block .item');
     });
